71956ea0e84a4bc4f9fda386604bf81589e63b68
waffledood <itshaikalhere@hotmail.sg>
2021-05-17 14:37:16 +0800

    Solved Problem 797

--- Graphs/797. All Paths From Source to Target/Solution.java
+++ Graphs/797. All Paths From Source to Target/Solution.java
@@ -1,11 +1,14 @@
+import java.util.*;
+import java.io.*; 
+
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
         
         /*
         Approach:
             Perform recursive DFS & stop when the target node is reached.
+        
         Pseudocode:
-
             Loop through graph array & find the target vertex  
 
             // Use the Adjacency List to perform recursive DFS.
@@ -22,15 +25,37 @@ class Solution {
                     add arr to list of ArrayList
         */
 
-        // visited array to track if the vertex has been visited
-        int[] visited = new int[n];
-        // 
+        // answer / store the list of possible paths from source to target vertex
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
 
+        // target node
+        int target = graph.length - 1;
         
+        // recursive DFS
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        arr.add(0);
+        recursiveDFS(graph, arr, 0, target, ans);
+
+        return ans;        
 
     }
 
-    public void recursiveDFS() {
-        //
+    public void recursiveDFS(int[][] graph, ArrayList<Integer> arr, int curr, int target, List<List<Integer>> ans) {
+
+        for (int i = 0; i < graph[curr].length; i++) {
+            ArrayList<Integer> arr2 = new ArrayList<Integer>(arr);
+            
+            int vertex = graph[curr][i];
+            
+            if (vertex == target) {
+                arr2.add(vertex);
+                ans.add(arr2);
+                //return;
+            } else {
+                arr2.add(vertex);
+                recursiveDFS(graph, arr2, vertex, target, ans);
+            }
+        }
+        
     }
 }
